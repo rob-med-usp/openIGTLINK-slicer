61cd2e6e21176a2f353a74083238bb0975c7afc8
José Carlos <josecarlos.101@usp.br>
2022-09-08 17:22:53 -0300

    registration output

--- registration/script.py
+++ registration/script.py
@@ -1,3 +1,130 @@
+# CODIGO ADAPTADO DE https://gist.github.com/ungi/d34094acb2a08601b51321d7334cf158
+# autor do codigo bruto: Tamas Ungi (https://gist.github.com/ungi)
+
+#OBS: Este codigo não executará caso não o abra em um ambiente do 3D Slicer devido aos modulos e funções deste
+# para executá-lo, use o start_script.sh (antes, altere o PATH para o diretório de seu 3D Slicer)
+
+# Create transform node for registration result (optional)
+
+import numpy
+import os
+from datetime import datetime
+
+
+
 alphaToBeta = slicer.vtkMRMLLinearTransformNode()
 alphaToBeta.SetName('AlphaToBeta')
 slicer.mrmlScene.AddNode(alphaToBeta)
+
+# Experiment parameters (start from here if you have alphaToBeta already)
+
+N = 10         # Number of fiducials
+Scale = 100.0  # Size of space where fiducial are placed
+Sigma = 2.0    # Radius of random error
+
+# Create first fiducial list
+
+fromNormCoordinates = numpy.random.rand(N, 3) # An array of random numbers
+noise = numpy.random.normal(0.0, Sigma, N*3)
+
+# Create the two fiducial lists
+
+alphaFids = slicer.vtkMRMLMarkupsFiducialNode()
+alphaFids.SetName('Alpha')
+slicer.mrmlScene.AddNode(alphaFids)
+
+betaFids = slicer.vtkMRMLMarkupsFiducialNode()
+betaFids.SetName('Beta')
+slicer.mrmlScene.AddNode(betaFids)
+betaFids.GetDisplayNode().SetSelectedColor(1,1,0)
+
+# vtkPoints type is needed for registration
+
+alphaPoints = vtk.vtkPoints()
+betaPoints = vtk.vtkPoints()
+
+for i in range(N):
+  x = (fromNormCoordinates[i, 0] - 0.5) * Scale
+  y = (fromNormCoordinates[i, 1] - 0.5) * Scale
+  z = (fromNormCoordinates[i, 2] - 0.5) * Scale
+  numFids = alphaFids.AddFiducial(x, y, z)
+  numPoints = alphaPoints.InsertNextPoint(x, y, z)
+  xx = x+noise[i*3]
+  yy = y+noise[i*3+1]
+  zz = z+noise[i*3+2]
+  numFids = betaFids.AddFiducial(xx, yy, zz)
+  numPoints = betaPoints.InsertNextPoint(xx, yy, zz)
+
+# Create landmark transform object that computes registration
+
+landmarkTransform = vtk.vtkLandmarkTransform()
+landmarkTransform.SetSourceLandmarks( alphaPoints )
+landmarkTransform.SetTargetLandmarks( betaPoints )
+landmarkTransform.SetModeToRigidBody()
+landmarkTransform.Update()
+
+alphaToBetaMatrix = vtk.vtkMatrix4x4()
+landmarkTransform.GetMatrix( alphaToBetaMatrix )
+
+det = alphaToBetaMatrix.Determinant()
+if det < 1e-8:
+  print('Unstable registration. Check input for collinear points.')
+
+alphaToBeta.SetMatrixTransformToParent(alphaToBetaMatrix)
+
+print(alphaToBetaMatrix) # retorna informações gerais sobre a matriz
+
+
+#print(alphaToBeta)
+
+
+# Compute average point distance after registration
+
+average = 0.0
+numbersSoFar = 0
+
+for i in range(N):
+  numbersSoFar = numbersSoFar + 1
+  a = alphaPoints.GetPoint(i)
+  pointA_Alpha = numpy.array(a)
+  pointA_Alpha = numpy.append(pointA_Alpha, 1)
+  pointA_Beta = alphaToBetaMatrix.MultiplyFloatPoint(pointA_Alpha)
+  b = betaPoints.GetPoint(i)
+  pointB_Beta = numpy.array(b)
+  pointB_Beta = numpy.append(pointB_Beta, 1)
+  distance = numpy.linalg.norm(pointA_Beta - pointB_Beta)
+  average = average + (distance - average) / numbersSoFar
+
+print("Average distance after registration: " + str(average))
+
+
+####################### saida em txt (armazenamento da matriz)
+
+txt = ''
+for i in range(4):
+    for j in range(4):
+      ele = alphaToBetaMatrix.GetElement(i, j)
+      print(ele) # retorna elemento por elemento
+      txt = f"{txt} {ele}"
+    txt = txt + "\n"
+
+
+now = datetime.now().time().strftime("%H:%M:%S") # time object
+date = datetime.now().strftime("%Y-%m-%d") # date object
+# print("date:",date)
+# print("time =", now)
+
+
+txt = txt + f'Matriz transformação gerada em -> Horário: {now}; Data (ano-mês-dia): {date}'
+print(txt)
+
+
+
+
+cur_path = os.path.dirname(__file__)
+new_path = cur_path + '/out/matrix-reg.txt'
+# new_path = os.path.relpath('/out/matrix-reg.txt', cur_path)
+
+with open(new_path, 'w') as file:
+  file.write(txt)
+      
--- start_script.sh
+++ start_script.sh
@@ -3,4 +3,8 @@
 # MEU PATH - PROTOTIPO (AUTOMATIZAR ESSA TAREFA)
 PATH_SLICER='Desktop/JoseCarlos/3d_slicer/Slicer-5.0.2-linux-amd64'
 
-exec ~/$PATH_SLICER/Slicer --no-main-window --python-script registration/script.py
+exec ~/$PATH_SLICER/Slicer --no-main-window --python-script registration/script.py 
+
+# --no-main_window
+# --show-python-interactor # para abrir somente o python interactor
+# --python-script registration/script.py # para scripts ja prontos
