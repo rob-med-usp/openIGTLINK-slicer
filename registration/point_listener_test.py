fd880aef7d9bf0bbea2c063d9fe17b152eb31700
José Carlos <josecarlos.101@usp.br>
2022-11-19 15:37:21 -0300

    yes

--- registration/point_listener_test.py
+++ registration/point_listener_test.py
@@ -1,45 +1,58 @@
+#!/usr/bin/env python3
+
 import rospy
 from geometry_msgs.msg import Point
 from ros_igtl_bridge.msg import igtlpoint
 import os
 
-txt_nome = "slicer_points.txt"
 
-def callback(data):
-    name = data.name
-    x = data.pointdata.x
-    y = data.pointdata.y
-    z = data.pointdata.z
 
-    id = name[-1:]
-    # rospy.loginfo("\x1b[H")
-    rospy.loginfo('PONTO RECEBIDO: Nome: %s; x: %f; y: %f; z: %f;', name, x, y, z)
-    msg = f"{x:.2f} {y:.2f} {z:.2f} {id}" # <-- Ajustar precisão do registro com essas casas decimais
-    print(msg + "\n")
+class Listener():
 
-    # a fazer:
-    # log com historico de pontos recebidos com horario e data (em local especifico)
+    def __init__(self):
+        self.txt_nome = "slicer_points.txt"
 
-    if id == "1":
-        open(txt_nome, "w").close() # apaga arquivo a cada recebimento dos pontos 
+    def callback(self, data):
+        name = data.name
+        x = data.pointdata.x
+        y = data.pointdata.y
+        z = data.pointdata.z
 
-    with open(txt_nome, "a") as file:
-        file.write(msg + "\n")
+        id = name[-1:]
+        # rospy.loginfo("\x1b[H")
+        rospy.loginfo('PONTO RECEBIDO: Nome: %s; x: %f; y: %f; z: %f;', name, x, y, z)
+        msg = f"{x:.2f} {y:.2f} {z:.2f} {id}" # <-- Ajustar precisão do registro com essas casas decimais
+        print(msg + "\n")
 
-   
+        # a fazer:
+        # log com historico de pontos recebidos com horario e data (em local especifico)
 
-def sub():
-    rospy.init_node('point_in', anonymous=True)
+        if id == "1":
+            open(self.txt_nome, "w").close() # apaga arquivo a cada recebimento dos pontos 
+
+        with open(self.txt_nome, "a") as file:
+            file.write(msg + "\n")
 
     
-    # file.write()
 
-    rospy.Subscriber('IGTL_POINT_IN', igtlpoint, callback)
+    def set_node_pub(self):
+        rospy.init_node('point_in', anonymous=True)
+
+        # file.write()
+
+        rospy.Subscriber('IGTL_POINT_IN', igtlpoint, self.callback)
     
+        rospy.spin()
+    
+    def flush(self):
+        with open(self.txt_nome, "w") as file:
+            file.flush()
 
-    rospy.spin()
+def main():
+
+    listener = Listener()
+    listener.flush()
+    listener.set_node_sub()
 
 if __name__ == "__main__":
-    with open(txt_nome, "w") as file:
-        file.flush()
-    sub()
+    main()
